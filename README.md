376dd2b8481c3349a22428509b713818e39d446e
Brandon Mulcahy <brandon@jangler.info>
2015-08-01 11:05:05 -0400

    Add roll program

--- README.md
+++ README.md
@@ -5,4 +5,5 @@ Miscellaneous public domain utility programs. See
 - `gifdrop` - drop frames from an (unoptimized) animated GIF
 - `hueview` - display colors
 - `itdump` - dump samples from an Impulse Tracker module
+- `roll` - D&D-style command-line dice roller
 - `tmpsrv` - serve files over HTTP
---
+++ roll/roll.go
@@ -0,0 +1,68 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"os"
+	"regexp"
+	"strconv"
+
+	"github.com/peterh/liner"
+)
+
+var diceRegexp = regexp.MustCompile(`^(\d+)d(\d+) *((-|\+) *(\d+))?$`)
+
+func main() {
+	line := liner.NewLiner()
+	defer line.Close()
+
+	line.SetCtrlCAborts(true)
+
+	for {
+		str, err := line.Prompt("> ")
+		if err == liner.ErrPromptAborted {
+			break
+		} else if err != nil {
+			fmt.Fprintln(os.Stderr, err)
+			break
+		} else if str == "" {
+			continue
+		}
+
+		submatch := diceRegexp.FindStringSubmatch(str)
+		if submatch == nil {
+			fmt.Fprintln(os.Stderr, "invalid syntax.")
+			continue
+		}
+		dice, err := strconv.ParseInt(submatch[1], 10, 0)
+		if err != nil {
+			fmt.Fprintln(os.Stderr, err)
+			continue
+		}
+		sides, err := strconv.ParseInt(submatch[2], 10, 0)
+		if err != nil {
+			fmt.Fprintln(os.Stderr, err)
+			continue
+		}
+
+		var total int64
+		if submatch[3] != "" {
+			bonus, err := strconv.ParseInt(submatch[5], 10, 0)
+			if err != nil {
+				fmt.Fprintln(os.Stderr, err)
+				continue
+			}
+			if submatch[4] == "+" {
+				total += bonus
+			} else {
+				total -= bonus
+			}
+		}
+		for i := 0; i < int(dice); i++ {
+			total += int64(rand.Intn(int(sides)) + 1)
+		}
+
+		fmt.Println(total)
+		line.AppendHistory(str)
+	}
+}
